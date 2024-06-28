a3194d061e1b614497c8e3888ad1bcfb405e2248
CharlesAduSark <charlesadusarkodie@gmail.com>
2024-06-28 09:37:36 +0000

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Recipes from "./pages/Recipes";
 import Welcome from "./pages/Welcome";
 import Categories from "./pages/Categories";
-import "./App.css";
+import "./App.css"
 
 const router = createBrowserRouter([
   { path: '/', element: <Welcome/>},
